073164e2dd8af94b6ca6bf6103a1a10e61c04006
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-10 12:06:48 -0600

    Create Env.java

---
+++ parser/Env.java
@@ -0,0 +1,22 @@
+
+package assign6.parser;
+import java.util.*; 
+import assign6.lexer.*; 
+
+public class Env {
+
+	public Hashtable<Node,Type> table;
+	protected Env prev;
+	public Env() { table = new Hashtable<Node,Type>();}
+	public Env(Env n) { table = new Hashtable<Node,Type>(); prev = n; }
+
+	public void put(Node w, Type i) { table.put(w, i); }
+
+	public Type get(String w) {
+		for( Env e = this; e != null; e = e.prev ) {
+			Type found = (Type)(e.table.get(w));
+			if( found != null ) return found;
+		}
+		return null;
+	}
+}
