326f3187528806569a2b090cb101cebcab66b608
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-12 17:23:54 -0600

    Update WhileNode.java

--- ast/WhileNode.java
+++ ast/WhileNode.java
@@ -5,7 +5,7 @@ import assign6.visitor.*;
 
 public class WhileNode extends StatementNode
 {
-    public ParenNode condition;
+    public Node condition;
     public StatementNode stmt;
 
     public WhileNode()
@@ -13,7 +13,7 @@ public class WhileNode extends StatementNode
 
     }
 
-    public WhileNode(ParenNode node, StatementNode stmt)
+    public WhileNode(Node node, StatementNode stmt)
     {
         this.condition = node;
         this.stmt = stmt;
