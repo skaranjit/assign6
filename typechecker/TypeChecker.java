fa018ae01d25f67a06926db6783694aec4aced34
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-12 19:26:37 -0600

    Update TypeChecker.java

--- typechecker/TypeChecker.java
+++ typechecker/TypeChecker.java
@@ -10,6 +10,7 @@ public class TypeChecker extends ASTVisitor
 {
     public Parser parser = null;
     public Env top = null;
+    public Type lhsExp = null;
     public Type rhsExp = null;
     public boolean hasbeenInitialized = false;
     public String[] initilizedTokens = new String[1000];
@@ -138,6 +139,7 @@ public class TypeChecker extends ASTVisitor
             error("Variable " + n.left.id +" has not been declared.");
         }
         left = top.table.get(n.left.id);
+	lhsExp = left;
 	System.out.println("Debug:" + left );
 	n.left.accept(this);
 	
@@ -170,6 +172,69 @@ public class TypeChecker extends ASTVisitor
 
     public void visit(BinExprNode n)
     {
+    	 if (n.left instanceof IdentifierNode)
+        {
+		Type right;
+		if(!(top.table.containsKey(((IdentifierNode)n.right).id)))
+		{
+			 error("Variable " + ((IdentifierNode)n.right).id +" has not been declared.");
+		}
+		right = top.table.get(((IdentifierNode)n.right).id);
+		if(lhsExp == right) ((IdentifierNode)n.right).accept(this);
+		else error("Type mismatch: "+ n.left.id+" of type " +left + " but " +((IdentifierNode)n.right).id + " of type " + right);
+         
+        }
+        else if (n.left instanceof NumNode)
+        {
+	    	if(lhsExp == Type.Int) ((NumNode)n.left).accept(this);
+		else error("Type mismatch: " + n.left.id+" of type " +left + " but " +((NumNode)n.right).value + " of type int");
+        }
+        else if (n.left instanceof RealNode)
+        {
+		if(lhsExp == Type.Int) ((RealNode)n.left).accept(this);
+		else error("Type mismatch: " + n.left.id+" of type " +left + " but " +((NumNode)n.right).value + " of type int");
+
+            
+        }
+        else if (n.left instanceof BooleanNode)
+        {
+            ((BooleanNode)n.left).accept(this);
+        }
+        else if (n.left instanceof ParenNode)
+        {
+            ((ParenNode)n.left).accept(this);
+        }
+        else
+            ((BinExprNode)n.left).accept(this);
+
+        
+
+        if (n.right != null)
+        {
+            if (n.right instanceof IdentifierNode)
+            {
+                ((IdentifierNode) n.right).accept(this);
+            }
+            else if (n.right instanceof NumNode)
+            {
+                ((NumNode) n.right).accept(this);
+            }
+            else if (n.right instanceof RealNode)
+            {
+                ((RealNode) n.right).accept(this);
+            }
+            else if (n.right instanceof BooleanNode)
+            {
+                ((BooleanNode) n.right).accept(this);
+            }
+            else if (n.right instanceof ParenNode)
+            {
+                ((ParenNode) n.right).accept(this);
+            }
+            else
+                ((BinExprNode) n.right).accept(this);
+
+        }
     }
 
     
