0643235337070c2bd3b55d116f34aa26c325c347
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-15 13:09:45 -0600

    Update ParenNode.java

--- ast/ParenNode.java
+++ ast/ParenNode.java
@@ -2,9 +2,9 @@ package assign6.ast;
 
 import assign6.visitor.ASTVisitor;
 
-public class ParenNode extends Node
+public class ParenNode extends ExprNode
 {
-    public Node node;
+    public ExprNode node;
 
     public ParenNode()
     {
