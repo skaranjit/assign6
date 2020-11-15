f3840917edf57e4d524c2a620ec67904fe710ecf
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-15 13:14:12 -0600

    Update NumNode.java

--- ast/NumNode.java
+++ ast/NumNode.java
@@ -30,35 +30,3 @@ public class NumNode extends ExprNode
     }
 
 }
-package assign6.ast;
-
-import assign6.lexer.*;
-import assign6.visitor.*;
-
-public class NumNode extends ExprNode
-{
-    public int value;
-    public Num v;
-
-    public NumNode()
-    {
-
-    }
-
-    public NumNode(Num v)
-    {
-        this.value = v.value;
-        this.v = v;
-    }
-
-    public void accept(ASTVisitor v)
-    {
-        v.visit(this);
-    }
-
-    public void printNode()
-    {
-        System.out.println("NumNode: " + value);
-    }
-
-}
