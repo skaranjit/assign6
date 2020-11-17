1e6cf82a6018ae067a3783d13e2118467effa32e
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-16 21:25:38 -0600

    Update Parser.java

--- parser/Parser.java
+++ parser/Parser.java
@@ -301,7 +301,7 @@ public class Parser extends ASTVisitor
         {
             rhs_assign = new ParenNode();
             ((ParenNode)rhs_assign).accept(this);
-            n.right.type = ((ParenNode)rhs_assign).type;
+            //n.right.type = ((ParenNode)rhs_assign).type;
         }
         if (look.tag == ';')
         {
