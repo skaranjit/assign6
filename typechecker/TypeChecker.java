8ad157ed5bbf57593283c404669c97125a601f52
Suman Karanjit <47037059+skaranjit@users.noreply.github.com>
2020-11-24 12:45:16 -0600

    Update TypeChecker.java

--- typechecker/TypeChecker.java
+++ typechecker/TypeChecker.java
@@ -317,7 +317,7 @@ public class TypeChecker extends ASTVisitor
     public void visit(IdentifierNode n)
     {
         System.out.println("visiting IdentifierNode");
-	//if(top.get(n.w) != null) println(n.w +" in symbol table");
+	if(top.get(n.w) != null) println(n.w +" in symbol table");
 	println (n.w + " in Identifier Node");
 // 		println("Variable is already declared: " +n.w);
 // 	}
